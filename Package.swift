b25bf07a2686840e16893c2d330c51db4794582d
Fred Rajaona <frajaona@gmail.com>
2016-10-16 19:53:05 +0200

    Fix Package.swift file

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "LiFXSwiftKit",
     dependencies: [
-    	.Package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", majorVersion: 7, minor: 4, patch: 3),
+        .Package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", majorVersion: 7, minor: 4),
     ]
 )
