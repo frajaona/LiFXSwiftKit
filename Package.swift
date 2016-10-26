d15d8cad5b43da5c35f3b9e0a22ec7443e4cbe52
Fred Rajaona <frajaona@gmail.com>
2016-10-26 23:20:28 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -5,5 +5,5 @@ let package = Package(
     dependencies: [
     	.Package(url: "https://github.com/frajaona/socks.git", majorVersion: 1, minor: 0),
     ],
-    exclude: ["Sources/LiFXCASSocket.swift", "Sources/UdpCASSocket.swift"]
+    exclude: ["Sources/LiFXCASSocket.swift", "Sources/UdpCASSocket.swift", "Sources/AppleScriptCommands"]
 )
